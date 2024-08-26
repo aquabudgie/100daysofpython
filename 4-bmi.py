232d10768d5006f620879cfc8a443d9010f11a00
asher <caleyad@gmail.com>
2024-08-26 12:13:16 +0800

    tip-calculator

---
+++ 4-bmi.py
@@ -0,0 +1,9 @@
+# calculates BMI based on hardcoded variables 
+height = 1.65 
+weight = 84
+
+# Write your code here.
+# Calculate the bmi using weight and height.
+bmi = weight / height ** 2
+
+print(round(bmi, 2))
