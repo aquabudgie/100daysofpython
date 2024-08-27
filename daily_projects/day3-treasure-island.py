39d36adbaec7a086dd4d0ce70137b0e39ac8a65c
asher <caleyad@gmail.com>
2024-08-27 15:42:40 +0800

    day3 treasure island

---
+++ daily_projects/day3-treasure-island.py
@@ -0,0 +1,121 @@
+# Treasure island choose your adventure game!
+
+import time
+
+def start_game():
+    print('''
+´´´´´´´´´´´´´´´´´´´ ¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶´´´´´´´´´´´´´´´´´´´`
+´´´´´´´´´´´´´´´´´¶¶¶¶¶¶´´´´´´´´´´´´´¶¶¶¶¶¶¶´´´´´´´´´´´´´´´´
+´´´´´´´´´´´´´´¶¶¶¶´´´´´´´´´´´´´´´´´´´´´´´¶¶¶¶´´´´´´´´´´´´´´
+´´´´´´´´´´´´´¶¶¶´´´´´´´´´´´´´´´´´´´´´´´´´´´´´¶¶´´´´´´´´´´´´
+´´´´´´´´´´´´¶¶´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´¶¶´´´´´´´´´´´
+´´´´´´´´´´´¶¶´´´´´´´´´´´´´´´´´´´´´`´´´´´´´´´´´¶¶´´´´´´´´´´`
+´´´´´´´´´´¶¶´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´¶¶´´´´´´´´´´
+´´´´´´´´´´¶¶´¶¶´´´´´´´´´´´´´´´´´´´´´´´´´´´´´¶¶´¶¶´´´´´´´´´´
+´´´´´´´´´´¶¶´¶¶´´´´´´´´´´´´´´´´´´´´´´´´´´´´´¶¶´´¶´´´´´´´´´´
+´´´´´´´´´´¶¶´¶¶´´´´´´´´´´´´´´´´´´´´´´´´´´´´´¶¶´´¶´´´´´´´´´´
+´´´´´´´´´´¶¶´´¶¶´´´´´´´´´´´´´´´´´´´´´´´´´´´´¶¶´¶¶´´´´´´´´´´
+´´´´´´´´´´¶¶´´¶¶´´´´´´´´´´´´´´´´´´´´´´´´´´´¶¶´´¶¶´´´´´´´´´´
+´´´´´´´´´´´¶¶´¶¶´´´¶¶¶¶¶¶¶¶´´´´´¶¶¶¶¶¶¶¶´´´¶¶´¶¶´´´´´´´´´´´
+´´´´´´´´´´´´¶¶¶¶´¶¶¶¶¶¶¶¶¶¶´´´´´¶¶¶¶¶¶¶¶¶¶´¶¶¶¶¶´´´´´´´´´´´
+´´´´´´´´´´´´´¶¶¶´¶¶¶¶¶¶¶¶¶¶´´´´´¶¶¶¶¶¶¶¶¶¶´¶¶¶´´´´´´´´´´´´´
+´´´´¶¶¶´´´´´´´¶¶´´¶¶¶¶¶¶¶¶´´´´´´´¶¶¶¶¶¶¶¶¶´´¶¶´´´´´´¶¶¶¶´´´
+´´´¶¶¶¶¶´´´´´¶¶´´´¶¶¶¶¶¶¶´´´¶¶¶´´´¶¶¶¶¶¶¶´´´¶¶´´´´´¶¶¶¶¶¶´´
+´´¶¶´´´¶¶´´´´¶¶´´´´´¶¶¶´´´´¶¶¶¶¶´´´´¶¶¶´´´´´¶¶´´´´¶¶´´´¶¶´´
+´¶¶¶´´´´¶¶¶¶´´¶¶´´´´´´´´´´¶¶¶¶¶¶¶´´´´´´´´´´¶¶´´¶¶¶¶´´´´¶¶¶´
+¶¶´´´´´´´´´¶¶¶¶¶¶¶¶´´´´´´´¶¶¶¶¶¶¶´´´´´´´¶¶¶¶¶¶¶¶¶´´´´´´´´¶¶
+¶¶¶¶¶¶¶¶¶´´´´´¶¶¶¶¶¶¶¶´´´´¶¶¶¶¶¶¶´´´´¶¶¶¶¶¶¶¶´´´´´´¶¶¶¶¶¶¶¶
+´´¶¶¶¶´¶¶¶¶¶´´´´´´¶¶¶¶¶´´´´´´´´´´´´´´¶¶¶´¶¶´´´´´¶¶¶¶¶¶´¶¶¶´
+´´´´´´´´´´¶¶¶¶¶¶´´¶¶¶´´¶¶´´´´´´´´´´´¶¶´´¶¶¶´´¶¶¶¶¶¶´´´´´´´´
+´´´´´´´´´´´´´´¶¶¶¶¶¶´¶¶´¶¶¶¶¶¶¶¶¶¶¶´¶¶´¶¶¶¶¶¶´´´´´´´´´´´´´´
+´´´´´´´´´´´´´´´´´´¶¶´¶¶´¶´¶´¶´¶´¶´¶´¶´¶´¶¶´´´´´´´´´´´´´´´´´
+´´´´´´´´´´´´´´´´¶¶¶¶´´¶´¶´¶´¶´¶´¶´¶´¶´´´¶¶¶¶¶´´´´´´´´´´´´´´
+´´´´´´´´´´´´¶¶¶¶¶´¶¶´´´¶¶¶¶¶¶¶¶¶¶¶¶¶´´´¶¶´¶¶¶¶¶´´´´´´´´´´´´
+´´´´¶¶¶¶¶¶¶¶¶¶´´´´´¶¶´´´´´´´´´´´´´´´´´¶¶´´´´´´¶¶¶¶¶¶¶¶¶´´´´
+´´´¶¶´´´´´´´´´´´¶¶¶¶¶¶¶´´´´´´´´´´´´´¶¶¶¶¶¶¶¶´´´´´´´´´´¶¶´´´
+´´´´¶¶¶´´´´´¶¶¶¶¶´´´´´¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶´´´´´¶¶¶¶¶´´´´´¶¶¶´´´´
+´´´´´´¶¶´´´¶¶¶´´´´´´´´´´´¶¶¶¶¶¶¶¶¶´´´´´´´´´´´¶¶¶´´´¶¶´´´´´´
+´´´´´´¶¶´´¶¶´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´¶¶´´¶¶´´´´´´
+´´´´´´´¶¶¶¶´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´¶¶¶¶´´´´´´´
+\n''')
+    print("Welcome to Treasure Island!\nYour mission is to find the treasure.")
+    encounter_list = ["fork_in_path","river_crossing","ruins_doors"]
+    for encounter in encounter_list:
+        print("\n")
+        win = process_encounter(encounter)
+    end_game(win)
+    return
+
+def end_game(win):
+    end_message = "Congratulations, you win!" if win is True else "GAME OVER"
+    play_again = input(f"{end_message}\nWould you like to play again, y or n? ")
+    if is_valid_yn(play_again):
+        if play_again.lower() == "y":
+            start_game()
+        else:
+            print("Thank you for playing!")
+            time.sleep(2)
+            exit()
+    return
+
+
+def process_encounter(encounter):
+    print(encounters[encounter]["prompt"])
+    options = encounters[encounter]["choices"].keys()
+    choice = process_input(options)
+    result = encounters[encounter]["choices"][choice]["result"]
+    print(f"\n{result}")
+    outcome = encounters[encounter]["choices"][choice]["correct"]
+    if outcome is True:
+        pass
+    else:
+        end_game(outcome)
+    return outcome
+
+
+def process_input(options):
+    input_request = ""
+    for i, option in enumerate(options):
+        if i == len(options)-1:
+            input_request += " or "
+        elif i > 0:
+            input_request += ", "
+        input_request += option
+    choice = input(f"Will you choose {input_request}? ")
+    if choice.lower() not in options:
+        print("Please choose a valid option.")
+        return process_input(options)
+    return choice
+
+def is_valid_yn(input):
+    if input.lower() in ("y", "n"):
+        return True
+    else:
+        return False
+
+if __name__ == "__main__":
+    encounters = {
+        "fork_in_path": {
+            "prompt":"You encounter a fork in the path, which way will you go?",
+            "choices": {
+                "left": {"result": "You safely continue on your path", "correct": True},
+                "right": {"result": "You are ambushed by an angered wild boar!", "correct": False}
+            }
+        },
+        "river_crossing": {
+            "prompt": "Ahead is a fast flowing river, nearby are wood planks that you could use to build a raft",
+            "choices": {
+                "swim": {"result": "You safely swim across.", "correct": True},
+                "raft": {"result": "Your raft breaks apart!", "correct": False}
+            }
+        },
+        "ruins_doors": {
+            "prompt":"You continue into a decrepit castle, inside which is a Red, Blue and Yellow door",
+            "choices": {
+                "red": {"result": "You find a hidden treasure!", "correct": True},
+                "yellow": {"result":"You fall into a spike trap!", "correct": False},
+                "blue": {"result":"The door opens, revealing a hungry bear!", "correct": False}
+            }
+        }
+    }
+    start_game()
