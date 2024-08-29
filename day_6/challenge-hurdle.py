6c94b175f6f9d5bc5bd00774faf473663a710a94
asher <caleyad@gmail.com>
2024-08-29 10:11:44 +0800

    day 6 lint

---
+++ day_6/challenge-hurdle.py
@@ -0,0 +1,39 @@
+x = 0
+y = 0
+
+
+def move():
+    x + 1
+
+
+def turn_left():
+    x + 1
+
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+
+def go_over_top():
+    turn_right()
+    move()
+    turn_right()
+
+
+def hurdle():
+    turn_left()
+    while wall_on_right():
+        move()
+    go_over_top()
+    while not wall_in_front():
+        move()
+    turn_left()
+
+
+while not at_goal():
+    if front_is_clear():
+        move()
+    else:
+        hurdle()
---
+++ day_6/maze.py
@@ -0,0 +1,50 @@
+x = 0
+
+
+def move():
+    x + 1
+
+
+def turn_left():
+    x + 1
+
+
+def front_is_clear():
+    return
+
+
+def right_is_clear():
+    return
+
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+    return
+
+
+def escape_right_loop():
+    if front_is_clear():
+        move()
+        return
+    else:
+        turn_left()
+        escape_right_loop()
+
+
+right_count = 0
+while not at_goal():
+    if right_is_clear():
+        right_count += 1
+        if right_count >= 4:
+            escape_right_loop()
+        else:
+            turn_right()
+            move()
+    elif front_is_clear():
+        move()
+        right_count = 0
+    else:
+        turn_left()
+        right_count = 0
