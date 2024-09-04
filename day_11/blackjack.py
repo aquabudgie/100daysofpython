62945bddd8c8147c3325a968e1ac10f7b161c5bb
asher <caleyad@gmail.com>
2024-09-04 13:35:00 +0800

    day 11 linted

--- day_11/blackjack.py
+++ day_11/blackjack.py
@@ -3,7 +3,15 @@ import random
 
 def print_final_hands(player_cards, player_score, computer_cards, computer_score):
     print(f"    Your final hand: {player_cards}, Your final score: {player_score}")
-    print(f"    Computer's final hand: {computer_cards}, Computer's final score: {computer_score}\n")
+    print(
+        f"    Computer's final hand: {computer_cards}, Computer's final score: {computer_score}\n"
+    )
+    return
+
+
+def print_current_hands(player_cards, player_score, computer_cards):
+    print(f"    Your current hand: {player_cards}, Your current score: {player_score}")
+    print(f"    Computer's first card: {computer_cards[0]}\n")
     return
 
 
@@ -21,7 +29,7 @@ def end_game(player_score, computer_score, player_cards, computer_cards):
         print("You went over, you lose!")
         main()
     while computer_score < 17:
-        computer_cards.append(random.choice(card_list))
+        computer_cards.append(deal_cards(1)[0])
         computer_score = sum(computer_cards)
     if computer_score > 21:
         print_final_hands(player_cards, player_score, computer_cards, computer_score)
@@ -46,31 +54,40 @@ def end_game(player_score, computer_score, player_cards, computer_cards):
     return
 
 
-def play_round():
-    player_cards = random.choices(card_list, k = 2)
+def deal_cards(k=2):
+    cards = random.choices(card_list, k=k)
+    return cards
+
+
+def initialise_cards():
+    player_cards = deal_cards()
     player_score = sum(player_cards)
-    computer_cards = random.choices(card_list, k = 2)
+    computer_cards = deal_cards()
     computer_score = sum(computer_cards)
-    print(f"    Your cards: {player_cards} current score: {player_score}")
-    print(f"    Computer's first card: {computer_cards[0]}")
+    return player_cards, player_score, computer_cards, computer_score
+
+
+def play_round():
+    player_cards, player_score, computer_cards, computer_score = initialise_cards()
+    print_current_hands(player_cards, player_score, computer_cards)
     while player_score < 21:
-        add_card = input("\nWould you like to get another card? Type 'y' or 'n': ").lower()
-        if add_card == 'y':
-            player_cards.append(random.choice(card_list))
+        add_card = input(
+            "\nWould you like to get another card? Type 'y' or 'n': "
+        ).lower()
+        if add_card == "y":
+            player_cards.append(deal_cards(1)[0])
             player_score = sum(player_cards)
-            print(f"    Your cards: {player_cards} current score: {player_score}")
+            print_current_hands(player_cards, player_score, computer_cards)
         else:
             end_game(player_score, computer_score, player_cards, computer_cards)
     end_game(player_score, computer_score, player_cards, computer_cards)
-
-
-    # print( if player_score >= 21:
-    # end_game
     return
 
 
 def main():
-    play_game = input("\nDo you want to play a game of Backjack? Type 'y' or 'n': ").lower()
+    play_game = input(
+        "\nDo you want to play a game of Backjack? Type 'y' or 'n': "
+    ).lower()
     if play_game == "y":
         play_round()
     else:
@@ -78,7 +95,7 @@ def main():
 
 
 if __name__ == "__main__":
-    card_list = [11,1,2,3,4,5,6,7,8,9,10,10,10,10]
+    card_list = [11, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
     player_cards = []
     player_score = 0
     computer_cards = []
